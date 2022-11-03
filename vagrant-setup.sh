5e16e2ca04155b237e29cd7545fedbbf73af0dfd
Tej Chajed <tchajed@vmware.com>
2022-11-03 14:12:09 -0700

    More fixes to vagrant setup

--- vagrant-setup.sh
+++ vagrant-setup.sh
@@ -11,8 +11,11 @@ sudo apt install -y python2 python-pip
 sudo apt install -y g++ cmake git pkg-config libssl-dev libreadline-dev
 
 if [ ! -d ~/ivy ]; then
-  git clone https://github.com/kenmcmil/ivy ~/ivy
+  git clone --recursive-submodules https://github.com/kenmcmil/ivy ~/ivy
 fi
 cd ~/ivy
-python2 setup.py develop --user
-
+if [ ! -f submodules/z3/build/libz3.so ]; then
+  sed -i "s|'python |'python2  " build_submodules.py
+  python2 build_submodules.py
+fi
+python2 setup.py install --user
