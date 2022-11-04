b0beb2ca9a6b8987e4b522c332112b111ba59c5c
Tej Chajed <tchajed@vmware.com>
2022-11-04 09:35:04 -0700

    Make a little more progress with basics

--- gen.py
+++ gen.py
@@ -24,3 +24,11 @@ print(f"invariant {some_pc}")
 for i in range(len(pcs)):
     for j in range(i + 1, len(pcs)):
         print(f"invariant ~{pcs[i]}(T) | ~{pcs[j]}(T)")
+
+def l2s(pc):
+    return f"($l2s_s T. {pc}(T))"
+
+print()
+print("# pcs don't change if a thread isn't scheduled")
+for pc in pcs:
+    print(f"invariant l2s_saved & ($l2s_w T. scheduled(T))(T) -> ({l2s(pc)}(T) <-> {pc}(T))")
